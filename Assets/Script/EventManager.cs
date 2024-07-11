413d2fc0e296d31533c2a52246da9f1607323e59
38798120ccm <38798120ccm@gmail.com>
2024-07-12 00:05:45 +0800

    Event done and TimeManager done

--- Assets/Script/EventManager.cs
+++ Assets/Script/EventManager.cs
@@ -7,13 +7,14 @@ public class EventManager : MonoBehaviour
     [SerializeField] Vector3 instantiatept;
     public List<Event> events_list;
     [SerializeField] TimeManager timeManager;
+    [SerializeField] GameManager gameManager;
     public List<Event> events_started;
-    public bool Inanimation;
+    public List<Event> events_stopping;
     public void StartEvents(Event e)
     {
-        if(e.CheckRequirement() == true && Inanimation == false)
+        if(e.CheckRequirement() == true && gameManager.Inanimation == false)
         {
-            Inanimation = true;
+            gameManager.Inanimation = true;
             events_list.Remove(e);
             GameObject eventGameOject = Instantiate(e.gameObject,instantiatept,Quaternion.identity);
             InitiateEvent(eventGameOject.GetComponent<Event>());
@@ -27,6 +28,16 @@ public class EventManager : MonoBehaviour
             e.DuringEvent();
         }
     }
+    public void EndEvent(Event e)
+    {
+        if(gameManager.Inanimation == false)
+        {
+            gameManager.Inanimation = true;
+            events_started.Remove(e);
+            events_stopping.Remove(e);
+            e.EndEvent();
+        }
+    }
     public void AddEvent(Event e)
     {   
         events_list.Add(e);
@@ -34,6 +45,8 @@ public class EventManager : MonoBehaviour
     public void InitiateEvent(Event e)
     {
         e.eventManager = this;
+        e.gameManager = gameManager;
+        e.timeManager = timeManager;
         e.MonthCode = timeManager.now.monthcode;
     }
 }
